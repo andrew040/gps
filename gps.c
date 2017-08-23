e74630837a64b39a022560c0440533efc15a08a8
andrew040 <31290684+andrew040@users.noreply.github.com>
2017-08-23 20:43:54 +0000

    Added position invalid check

--- gps.c
+++ gps.c
@@ -2,11 +2,10 @@
 #include <ctype.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>			//Used for UART
-#include <fcntl.h>			//Used for UART
-#include <termios.h>		//Used for UART
+#include <unistd.h>
+#include <fcntl.h>
+#include <termios.h>
 
-// nu heb ik dit toegevoegd
 char *strdup(const char *src)
 {
     char *tmp = malloc(strlen(src) + 1);
@@ -16,7 +15,7 @@ char *strdup(const char *src)
 }
 
 void explode(const char *src, const char *tokens, char ***list, size_t *len)
-{   
+{
     if(src == NULL || list == NULL || len == NULL)
         return;
 
@@ -43,7 +42,7 @@ void explode(const char *src, const char *tokens, char ***list, size_t *len)
 
 
     while((str = strtok(NULL, tokens)))
-    {   
+    {
         tmp = realloc(_list, (sizeof *_list) * (*len + 1));
         if(tmp == NULL)
             goto free_and_exit;
@@ -66,30 +65,11 @@ free_and_exit:
 
 int main (void)
 {
-
-	//-------------------------
-	//----- SETUP USART 0 -----
-	//-------------------------
-	//At bootup, pins 8 and 10 are already set to UART0_TXD, UART0_RXD (ie the alt0 function) respectively
 	int uart0_filestream = -1;
-	 
-	//OPEN THE UART
-	//The flags (defined in fcntl.h):
-	//	Access modes (use 1 of these):
-	//	O_RDONLY - Open for reading only.
-	//	O_RDWR - Open for reading and writing.
-	//	O_WRONLY - Open for writing only.
-	//
-	//	O_NDELAY / O_NONBLOCK (same function) - Enables nonblocking mode. When set read requests on the file can return immediately with a failure status
-	//	if there is no input immediately available (instead of blocking). Likewise, write requests can also return
-	//	immediately with a failure status if the output can't be written immediately.
-	//
-	//	O_NOCTTY - When set and path identifies a terminal device, open() shall not cause the terminal device to become the controlling terminal for the process.
 	close(uart0_filestream);
 	uart0_filestream = open("/dev/ttyAMA0", O_RDONLY | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
 	if (uart0_filestream == -1)
 	{
-		//ERROR - CAN'T OPEN SERIAL PORT
 		printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
 	}else{
 		printf("Port opened \n");
@@ -98,27 +78,26 @@ int main (void)
 	struct termios options;
 	tcgetattr(uart0_filestream, &options);
 	options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;		//<Set baud rate
-	options.c_iflag = IGNPAR;
+	options.c_iflag = IGNPAR;				//Ignore parity
 	options.c_oflag = 0;
-	options.c_lflag |= ICANON;
+	options.c_lflag |= ICANON;				//Use UART in line-mode, otherwise it just reads 8 bytes
 	tcflush(uart0_filestream, TCIFLUSH);
 	tcsetattr(uart0_filestream, TCSANOW, &options);
 
 
 	while(1){
-	//----- CHECK FOR ANY RX BYTES -----
 	if (uart0_filestream != -1)
 	{
-		char rx_buffer[256];  //Complete NMEA string
-		char *match;          //if GPRMC is found in NMEA string, this is non zero.
-		char posfix[] = "A";  //Position valid string to compare against.
-		int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
+		char rx_buffer[256];  				//Complete NMEA string
+		char *match;          				//if GPRMC is found in NMEA string, this is non zero.
+		char posfix[] = "A";  				//Position valid string to compare against.
+		int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);
 
 		FILE * fp;
 
 		if (rx_length < 0)
 		{
-			//printf("Error ocurred: %i bytes read\n", rx_length);	//An error occured (will occur if there are no bytes)
+			//Error
 		}
 		else if (rx_length == 0)
 		{
@@ -127,14 +106,13 @@ int main (void)
 		else
 		{
 			//Bytes received, terminate string with null
-			rx_buffer[rx_length] = '\0'; 
+			rx_buffer[rx_length] = '\0';
 			match = strstr (rx_buffer,"GPRMC");
 			if(match){
 				char **list;
 				size_t i, len;
 				explode(rx_buffer, ",", &list, &len);
-				
-				
+
 				/*
 				0: $GPRMC
 				1: 115206.000
@@ -148,17 +126,16 @@ int main (void)
 				9: 190817
 				10: A*52
 				*/
-				
-				printf("Test: %s", list[2]);
-				if(strcmp(list[2],posfix)){
+
+				if(!strcmp(list[2],posfix)){
 					fp = fopen ("file.txt", "a");
 					printf("DTG:%s/%sZ*POS:%s%s %s%s*SPD:%s\n",list[9],list[1],list[4],list[3],list[6],list[5],list[7]);
 					fprintf(fp,"%s;%s;%s;%s;%s;%s;%s\r\n",list[9],list[1],list[4],list[3],list[6],list[5],list[7]);
 					fclose(fp);
 				} else{
-					printf("POSITION INVALID");
+					printf("POSITION INVALID\n");
 				}
-				
+
 				/* free list */
 				for(i = 0; i < len; ++i)
 					free(list[i]);
@@ -170,8 +147,5 @@ int main (void)
 	}
 	//----- CLOSE THE UART -----
 	close(uart0_filestream);
-
-
-  printf ("Hello, world!\n");
   return 0;
 }
