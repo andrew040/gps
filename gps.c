75467cb7d39b43e63aaeaef5fa39e737ec890f71
andrew040 <31290684+andrew040@users.noreply.github.com>
2017-08-23 20:04:20 +0000

    Cleaned up code

--- gps.c
+++ gps.c
@@ -1,8 +1,7 @@
- #include <stdio.h>
- #include <ctype.h>
- #include <stdlib.h>
- #include <string.h>
-
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
 #include <unistd.h>			//Used for UART
 #include <fcntl.h>			//Used for UART
 #include <termios.h>		//Used for UART
@@ -93,19 +92,9 @@ int main (void)
 		//ERROR - CAN'T OPEN SERIAL PORT
 		printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
 	}else{
-		printf("DTG:******/******.000Z*POS:N****.**** E*****.*****SPD:*.**\n");
+		printf("Port opened \n");
 	}
-	
-	//CONFIGURE THE UART
-	//The flags (defined in /usr/include/termios.h - see http://pubs.opengroup.org/onlinepubs/007908799/xsh/termios.h.html):
-	//	Baud rate:- B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800, B500000, B576000, B921600, B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000
-	//	CSIZE:- CS5, CS6, CS7, CS8
-	//	CLOCAL - Ignore modem status lines
-	//	CREAD - Enable receiver
-	//	IGNPAR = Ignore characters with parity errors
-	//	ICRNL - Map CR to NL on input (Use for ASCII comms where you want to auto correct end of line characters - don't use for bianry comms!)
-	//	PARENB - Parity enable
-	//	PARODD - Odd parity (else even)
+
 	struct termios options;
 	tcgetattr(uart0_filestream, &options);
 	options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;		//<Set baud rate
@@ -115,37 +104,14 @@ int main (void)
 	tcflush(uart0_filestream, TCIFLUSH);
 	tcsetattr(uart0_filestream, TCSANOW, &options);
 
-        /*
-	//----- TX BYTES -----
-	unsigned char tx_buffer[20];
-	unsigned char *p_tx_buffer;
-	
-	p_tx_buffer = &tx_buffer[0];
-	*p_tx_buffer++ = 'H';
-	*p_tx_buffer++ = 'e';
-	*p_tx_buffer++ = 'l';
-	*p_tx_buffer++ = 'l';
-	*p_tx_buffer++ = 'o';
-	
-	if (uart0_filestream != -1)
-	{
-		int count = write(uart0_filestream, &tx_buffer[0], (p_tx_buffer - &tx_buffer[0]));		//Filestream, bytes to write, number of bytes to write
-		if (count < 0)
-		{
-			printf("UART TX error\n");
-		}
-	}
-
-	*/
-
 
 	while(1){
 	//----- CHECK FOR ANY RX BYTES -----
 	if (uart0_filestream != -1)
 	{
-		// Read up to 255 characters from the port if they are there
-		unsigned char rx_buffer[256];
-		unsigned char * match;
+		char rx_buffer[256];  //Complete NMEA string
+		char *match;          //if GPRMC is found in NMEA string, this is non zero.
+		char posfix[] = "A";  //Position valid string to compare against.
 		int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
 
 		FILE * fp;
@@ -183,12 +149,12 @@ int main (void)
 				10: A*52
 				*/
 				
-				if(strcmp(list[2],"A")){
+				printf("Test: %s", list[2]);
+				if(strcmp(list[2],posfix)){
 					fp = fopen ("file.txt", "a");
 					printf("DTG:%s/%sZ*POS:%s%s %s%s*SPD:%s\n",list[9],list[1],list[4],list[3],list[6],list[5],list[7]);
 					fprintf(fp,"%s;%s;%s;%s;%s;%s;%s\r\n",list[9],list[1],list[4],list[3],list[6],list[5],list[7]);
 					fclose(fp);
-				}
 				} else{
 					printf("POSITION INVALID");
 				}
